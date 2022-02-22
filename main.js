33e1cc20e999c1fa8d68c223bc85330e1ec70c58
Martina Vokáčová <martinavokacova@gmail.com>
2022-02-22 19:28:51 +0100

    fce hazej

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
 // tady je místo pro náš program
+
+
+function hazej() {
+  let kostka = document.querySelector("#kostka");
+  let hod = Math.floor(Math.random() * 7);
+  let zprava = document.querySelector('#zprava');
+  zprava.textContent = "Padla " + hod + ", házej znovu!"
+  console.log(hod);
+
+  if (hod === 1) {
+    kostka.src = "obrazky/1.png";
+  } else if (hod === 2) {
+    kostka.src = "obrazky/2.png";
+  } else if (hod === 3) {
+    kostka.src = "obrazky/3.png";
+  } else if (hod === 4) {
+    kostka.src = "obrazky/4.png";
+  } else if (hod === 5) {
+    kostka.src = "obrazky/5.png";
+  } else {
+    kostka.src = "obrazky/6.png";
+    zprava.textContent = "Jupí! Padla " + hod;
+  }
+}
